56db2bda4568dc64739edc2a328a5299cea4cf78
Unknown <tomatoesneverlie@gmail.com>
2021-09-09 16:43:34 -0400

    working hang-skip
    good god damn this is janky, will fix later.

--- execute.py
+++ execute.py
@@ -2,7 +2,7 @@
 # import os
 # 
 # # Regex expressions for identifying the start and end of code-blocks
-# block_start = re.compile("^```python#run(#\w*)*$");
+# block_start = re.compile("^```python#run(#\w=*)*$");
 # block_end = re.compile("^```$");
 # 
 # # Define a structure to keep track of codeblocks
@@ -46,22 +46,52 @@
 # 	for line in lines:
 # 		dest.write(line)
 
+import sys
 import subprocess
-proc = subprocess.Popen(['python3', '-i'],
+from threading  import Thread
+from queue import Queue, Empty
+
+def enqueue_output(out, queue):
+    for line in iter(out.readline, b''):
+        queue.put(line)
+    out.close()
+
+proc = subprocess.Popen(['python3', '-i', '-q'],
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
 
+qo = Queue()
+to = Thread(target=enqueue_output, args=(proc.stdout, qo), daemon=True)
+to.start()
+
+
+qe = Queue()
+te = Thread(target=enqueue_output, args=(proc.stderr, qe), daemon=True)
+te.start()
+
 lines = ["print(2+4)",
-		"print('test')",
+		"print('test 1')",
 		"x=1",
 		"print('test 2')",
+		"print(y)",
 		"print(200-x)"]
 
 for line in lines:
 	proc.stdin.write(f"{line.strip()}\n".encode("utf-8"))
 	proc.stdin.flush()
-	print(proc.stdout.readline().decode('utf-8'), end='')
+	try:  line = qo.get(timeout=.1)
+	except Empty:
+		print('no output yet')
+	else:
+		print(line.decode('utf-8'), end='')
+
+while True:
+	try:  line = qe.get_nowait()
+	except Empty:
+		break
+	else:
+		print(line.decode('utf-8'), end='')
 
 proc.stdin.close()
 proc.terminate()
