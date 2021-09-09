19ae3b4a4a569987faec5791b864a7a6c24e8f93
Unknown <tomatoesneverlie@gmail.com>
2021-09-09 15:58:15 -0400

    made example blocking

--- execute.py
+++ execute.py
@@ -52,15 +52,16 @@ proc = subprocess.Popen(['python3', '-i'],
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
 
-# To avoid deadlocks: careful to: add \n to output, flush output, use
-# readline() rather than read()
-proc.stdin.write(b'2+2\n')
-proc.stdin.flush()
-print(proc.stdout.readline())
+lines = ["print(2+4)",
+		"print('test')",
+		"x=1",
+		"print('test 2')",
+		"print(200-x)"]
 
-proc.stdin.write(b'len("foobar")\n')
-proc.stdin.flush()
-print(proc.stdout.readline())
+for line in lines:
+	proc.stdin.write(f"{line.strip()}\n".encode("utf-8"))
+	proc.stdin.flush()
+	print(proc.stdout.readline().decode('utf-8'), end='')
 
 proc.stdin.close()
 proc.terminate()
