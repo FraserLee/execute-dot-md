2abe7a5eaaf587e2a404735a7fd2815d1365a61a
Unknown <tomatoesneverlie@gmail.com>
2021-09-09 15:49:18 -0400

    added very basic subprocess interaction
    https://eli.thegreenplace.net/2017/interacting-with-a-long-running-child-process-in-python/

--- execute.py
+++ execute.py
@@ -1,47 +1,67 @@
-import re
-import os
+# import re
+# import os
+# 
+# # Regex expressions for identifying the start and end of code-blocks
+# block_start = re.compile("^```python#run(#\w*)*$");
+# block_end = re.compile("^```$");
+# 
+# # Define a structure to keep track of codeblocks
+# class block:
+# 	# include a language enum if extended later
+# 	startline: int
+# 	endline = None
+# 	unboxed = False
+# 	hide    = False
+# 	new     = False
+# 	def __init__(self, startline):
+# 		self.startline = startline
+# 		self.lines   = []
+# 
+# # Extraction of blocks and lines from a file
+# lines = []
+# codeblocks = []
+# 
+# with open('TestCases.md', 'r') as source:
+# 	current_block = None
+# 	for i, line in enumerate(source):
+# 
+# 		lines.append(line)
+# 
+# 		if current_block is None and block_start.match(line):
+# 				current_block = block(i+1)
+# 
+# 		elif current_block is not None:
+# 			if block_end.match(line):
+# 				current_block.endline = i-1
+# 				codeblocks.append(current_block)
+# 				current_block = None
+# 			else: current_block.lines.append(line)
+# 
+# # Execute each codeblock
+# for i in codeblocks:
+# 	print(i.startline, i.endline, i.lines)
+# 
+# # Output the results
+# with open('Output.md', 'w') as dest:
+# 	for line in lines:
+# 		dest.write(line)
 
-# Regex expressions for identifying the start and end of code-blocks
-block_start = re.compile("^```python#run(#\w*)*$");
-block_end = re.compile("^```$");
+import subprocess
+proc = subprocess.Popen(['python3', '-i'],
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
 
-# Define a structure to keep track of codeblocks
-class block:
-	# include a language enum if extended later
-	startline: int
-	endline = None
-	unboxed = False
-	hide    = False
-	new     = False
-	def __init__(self, startline):
-		self.startline = startline
-		self.lines   = []
+# To avoid deadlocks: careful to: add \n to output, flush output, use
+# readline() rather than read()
+proc.stdin.write(b'2+2\n')
+proc.stdin.flush()
+print(proc.stdout.readline())
 
-# Extraction of blocks and lines from a file
-lines = []
-codeblocks = []
+proc.stdin.write(b'len("foobar")\n')
+proc.stdin.flush()
+print(proc.stdout.readline())
 
-with open('TestCases.md', 'r') as source:
-	current_block = None
-	for i, line in enumerate(source):
-
-		lines.append(line)
-
-		if current_block is None and block_start.match(line):
-				current_block = block(i+1)
-
-		elif current_block is not None:
-			if block_end.match(line):
-				current_block.endline = i-1
-				codeblocks.append(current_block)
-				current_block = None
-			else: current_block.lines.append(line)
-
-# Execute each codeblock
-for i in codeblocks:
-	print(i.startline, i.endline, i.lines)
-
-# Output the results
-with open('Output.md', 'w') as dest:
-	for line in lines:
-		dest.write(line)
+proc.stdin.close()
+proc.terminate()
+proc.wait(timeout=0.2)
