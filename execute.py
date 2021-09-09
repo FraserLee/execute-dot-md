f6c384d6a04e2360fd03d34a26db5aa34df612d7
Unknown <tomatoesneverlie@gmail.com>
2021-09-09 14:23:59 -0400

    slightly refactored stuff

--- execute.py
+++ execute.py
@@ -1,34 +1,34 @@
 import re
 
-source = open('TestCases.md', 'r')
-lines = []
-for line in source:
-	lines.append(line)
-source.close()
-
-
-
+# Regex expressions for identifying the start and end of code-blocks
 CB_start = re.compile("^```python#run(#\w*)*$");
 CB_end = re.compile("^```$");
 
+# Extraction of blocks and lines from a file
+lines = []
 codeblocks = []
 
-in_codeblock = False
-cb_startline = 0
-for i, line in enumerate(lines):
-	if not in_codeblock:
-		if CB_start.match(line):
-			cb_startline = i+1
-			in_codeblock = True
-	else:
-		if CB_end.match(line):
-			codeblocks.append((cb_startline, i-1))
-			in_codeblock = False
+with open('TestCases.md', 'r') as source:
+	in_codeblock = False
+	cb_startline = 0
+	for i, line in enumerate(source):
 
+		lines.append(line)
+
+		if not in_codeblock:
+			if CB_start.match(line):
+				cb_startline = i+1
+				in_codeblock = True
+		else:
+			if CB_end.match(line):
+				codeblocks.append((cb_startline, i-1))
+				in_codeblock = False
+
+# Execute each codeblock
 for i in codeblocks:
 	print(i)
 
-dest = open('Output.md', 'w')
-for line in lines:
-	dest.write(line)
-dest.close()
+# Output the results
+with open('Output.md', 'w') as dest:
+	for line in lines:
+		dest.write(line)
